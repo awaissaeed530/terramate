207131abacd693dc9a0d65822ea9a994333b00a7
Awais Saeed <dev@awaissaeed.com>
2026-06-07 00:47:47 +0500

    add mastra

---
+++ src/mastra/index.ts
@@ -0,0 +1,11 @@
+import { Mastra } from '@mastra/core'
+import { LibSQLStore } from "@mastra/libsql";
+import { weatherAgent } from './agents/weather-agent'
+
+export const mastra = new Mastra({
+    agents: { weatherAgent },
+    storage: new LibSQLStore({
+        id: 'mastra-storage',
+        url: 'file:./mastra.db',
+    }),
+})
